7fae234a6e22e46afba9958e81b00825da5732c5
Mijael Viricochea Parra <mijael.viricochea@outlook.com>
2021-02-03 00:31:42 -0400

    Screen orientation changed to portrait

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         applicationId = "com.alphemsoft.education.regression"
         minSdkVersion(21)
         targetSdkVersion(30)
-        versionCode = 17
+        versionCode = 18
         versionName = "3.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
